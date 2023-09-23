59b4e4d427031003e65302c232ebe270603718f7
jackytsheng <jackytsheng@gmail.com>
2023-09-23 17:02:45 +1000

    first commit

---
+++ src/app/NavBar.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link"
+
+
+export default function NavBar() {
+  return (
+    <nav className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <a href="https://google.com" className="flex items-center">
+        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
+        <span className="self-center text-2xl font-semibold whitespace-nowrapd">Property Score</span>
+      </a>
+      <div id="navbar-default">
+        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white ">
+          <li>
+            <Link href="/login" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Log in</Link>
+          </li>
+          <li>
+            <Link href="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</Link>
+          </li>
+          <li>
+            <Link href="/summary" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:bg-transparent">Summary</Link>
+          </li>
+          <li>
+            <Link href="/result" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Result</Link>
+          </li>
+          <li>
+            <Link href="/feedback" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Feedback</Link>
+          </li>
+        </ul>
+      </div>
+
+    </nav >
+  )
+}
---
+++ src/app/summary/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Summary() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Summary
+    </main>
+  )
+}
